4c4815c8e86d71d9b9ac2847d3999df9211d730a
Juan Lagunas <dev.jlagunas@gmail.com>
2024-06-21 16:56:00 +0900

    add HeaderComponent to app.vue and connect with emits and props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { ref } from "vue";
+import Navbar from "./components/NavbarComponent.vue";
+import Sidebar from "./components/SidebarComponent.vue";
+import Header from "./components/HeaderComponent.vue";
+//State
+const toggleSidebar = ref(false);
+const isDarkMode = ref(false);
+
+//Methods
+const toggleDarkMode = () => {
+  isDarkMode.value = !isDarkMode.value;
+  document.documentElement.classList.toggle("dark", isDarkMode.value);
+};
 </script>
 
 <template>
-  <RouterView />
+  <div class="dark:bg-slate-900">
+    <Header @darkModeToggled="toggleDarkMode" :isDarkMode="isDarkMode" />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["darkModeToggled"]);
+
+const toggleDarkMode = () => {
+  emit("darkModeToggled");
+};
+
+//Props
+const props = defineProps({
+  isDarkMode: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+console.log(props.isDarkMode);
+</script>
+
+<template>
+  <header class="flex justify-evenly items-center">
+    <button @click="toggleDarkMode">
+      <i
+        :class="isDarkMode ? 'fa-solid fa-sun' : 'fa-solid fa-moon'"
+        class="text-2xl text-slate-600 dark:text-slate-200"
+      ></i>
+    </button>
+    <p class="font-pacifico text-emerald-500 text-5xl">
+      Hello<span class="text-5xl text-orange-400">!</span>
+    </p>
+    <div
+      class="flex flex-col gap-1 text-xs text-slate-600 dark:text-slate-200 divide-y-2 divide-slate-200"
+    >
+      <button>EN</button>
+      <button class="md:pl-1 pt-1 md:pt-0">JP</button>
+      <button class="md:pl-1 pt-1 md:pt-0">ES</button>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
