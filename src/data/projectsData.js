7c1b139771d804d1fe89fba469c4c4a6a8618303
Juan Lagunas <dev.jlagunas@gmail.com>
2024-06-24 14:35:25 +0900

    add project data to projectsData.js file

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,21 +1,142 @@
 export const projects = [
   {
     id: 1,
-    name: "Project Name",
-    tagline: "A quick tagline description of my project goes here",
-    description: "A longer detailed description of the project.",
+    name: "Cafe Triangle",
+    tagline: "Cafe Triangle was a small cafe located in Kanazawa, Japan",
+    description: `Cafe Triangle was the first fully functioning hosted website I ever made.
+    When I first started learning web dev I made it my one year goal to create our shops website. This is the final project.
+    I especially love the vibrant but subdued user interface
+    `,
     images: {
       desktop: [
-        "desktop_image1.jpg",
-        "desktop_image2.jpg",
-        "desktop_image3.jpg",
+        "@/assets/screenshots/cafe-desktop-shot-1.png",
+        "@/assets/screenshots/cafe-desktop-shot-2.png",
+        "@/assets/screenshots/cafe-desktop-shot-3.png",
       ],
-      mobile: ["mobile_image1.jpg", "mobile_image2.jpg", "mobile_image3.jpg"],
+      mobile: [
+        "@/assets/screenshots/cafe-mobile-shot-1.png",
+        "@/assets/screenshots/cafe-mobile-shot-2.png",
+        "@/assets/screenshots/cafe-mobile-shot-3.png",
+      ],
     },
-    features: ["Feature 1", "Feature 2", "Feature 3"],
-    technologies: ["Vue", "Tailwind CSS", "GSAP"],
-    github: "https://github.com/yourusername/projectname",
-    liveDemo: "https://yourlivedemo.com",
+    technologies: ["Angular", "Tailwind CSS", "Flowbite"],
+    features: [
+      "Multi-lingual functionality for EN/JP/ES",
+      "Dark/Light Modes",
+      "Responsive Design",
+      "Optimized for Speed",
+    ],
+    github: "https://github.com/dev-jLagunas/cafe-refresh",
+    liveDemo: "https://cafe-triangle.com/",
+    codeSample: {
+      images: ["code_image1.jpg", "code_image2.jpg"],
+      explanation:
+        "This code snippet shows how we initialize the main Vue instance and configure the router. The second image shows the setup of the state management using Vuex.",
+    },
+  },
+  {
+    id: 2,
+    name: "Code Chicano",
+    tagline:
+      "A crud blog about my experience as a Mexican American self-taught programmer",
+    description: `Code Chicano is a fully functioning C.R.U.D application with Firebase as its backend. It is also made with a quill editor so
+    I am able to login and write new blog posts any time I want. The blog is fully responsive with many great features and funtionality.
+    `,
+    images: {
+      desktop: [
+        "@/assets/screenshots/chicano-desktop-shot-1.png",
+        "@/assets/screenshots/chicano-desktop-shot-2.png",
+        "@/assets/screenshots/chicano-desktop-shot-3.png",
+      ],
+      mobile: [
+        "@/assets/screenshots/chicano-mobile-shot-1.png",
+        "@/assets/screenshots/chicano-mobile-shot-2.png",
+        "@/assets/screenshots/chicano-mobile-shot-3.png",
+      ],
+    },
+    technologies: ["Angular", "Angular Material", "SCSS", "Firebase"],
+    features: [
+      "Firebase Authentication",
+      "Firebase Database",
+      "Quill Editor",
+      "A.I Generated Artwork",
+      "Dark/Light Modes",
+      "Responsive Design",
+      "Custom U.I. Components",
+    ],
+    github: "https://github.com/dev-jLagunas/code-chicano",
+    liveDemo: "https://code-chicano.netlify.app/",
+    codeSample: {
+      images: ["code_image1.jpg", "code_image2.jpg"],
+      explanation:
+        "This code snippet shows how we initialize the main Vue instance and configure the router. The second image shows the setup of the state management using Vuex.",
+    },
+  },
+  {
+    id: 3,
+    name: "Cosmic Neighborhood",
+    tagline:
+      "Cosmic Neighborhood is an immersive and interactive space-themed website",
+    description: `Cosmic Neighborhood is an expanded and enhanced version of a Frontend Mentor challenge. 
+    Users can explore the planets of our solar system in an interactive and informative
+    way. One of the standout features of this project is the interactive 3D models of the planets, created using Three.js. 
+    Users can zoom in, view the planets from different angles, and engage with the models for a more dynamic experience.
+    `,
+    images: {
+      desktop: [
+        "@/assets/screenshots/cosmic-desktop-shot-1.png",
+        "@/assets/screenshots/cosmic-desktop-shot-2.png",
+        "@/assets/screenshots/cosmic-desktop-shot-3.png",
+      ],
+      mobile: [
+        "@/assets/screenshots/cosmic-mobile-shot-1.png",
+        "@/assets/screenshots/cosmic-mobile-shot-2.png",
+        "@/assets/screenshots/cosmic-mobile-shot-3.png",
+      ],
+    },
+    technologies: ["Vue", "Tailwind CSS", "Three Js"],
+    features: [
+      "Three JS 3D Models of Planets",
+      "Beautiful user interface",
+      "css animations",
+    ],
+    github: "https://github.com/dev-jLagunas/cosmic-neighborhood",
+    liveDemo: "https://dev-jlagunas.github.io/cosmic-neighborhood/#/",
+    codeSample: {
+      images: ["code_image1.jpg", "code_image2.jpg"],
+      explanation:
+        "This code snippet shows how we initialize the main Vue instance and configure the router. The second image shows the setup of the state management using Vuex.",
+    },
+  },
+  {
+    id: 4,
+    name: "Horizontal Scroll Gallery",
+    tagline:
+      "A horizontal scroll gallery filled with scroll triggered animations",
+    description: `This was more of a passion project. For some reason I alway found horizontal scroll websites to be lovely.
+    So I decided to showcase my photography using GSAP scroll trigger and horizontal scroll which was much tricker than I expected.
+    The project was very time consuming but worth it in the end. 
+    `,
+    images: {
+      desktop: [
+        "@/assets/screenshots/gallery-desktop-shot-1.png",
+        "@/assets/screenshots/gallery-desktop-shot-2.png",
+        "@/assets/screenshots/gallery-desktop-shot-3.png",
+      ],
+      mobile: [
+        "@/assets/screenshots/gallery-mobile-shot-1.png",
+        "@/assets/screenshots/gallery-mobile-shot-2.png",
+        "@/assets/screenshots/gallery-mobile-shot-3.png",
+      ],
+    },
+    technologies: ["Vite", "Vanilla Javascript", "GSAP Scroll Trigger"],
+    features: [
+      "complex scroll trigger animations",
+      "responsive design",
+      "beautiful user interface",
+    ],
+    github: "https://github.com/dev-jLagunas/horizontal-gallery",
+    liveDemo: "https://dev-jlagunas.github.io/horizontal-gallery/",
     codeSample: {
       images: ["code_image1.jpg", "code_image2.jpg"],
       explanation:
