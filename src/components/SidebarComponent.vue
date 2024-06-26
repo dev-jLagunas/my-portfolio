23fb7df500b151b03c1464675e86184dd8294239
Juan Lagunas <dev.jlagunas@gmail.com>
2024-06-26 17:58:10 +0900

    refactor(SidebarComponent): simplify Tailwind CSS classes using @apply directive

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useRouter } from "vue-router";
+
 // Emits
 const emit = defineEmits(["closeSidebar", "darkModeToggled"]);
 
@@ -21,45 +22,31 @@ const navigateTo = (path) => {
 </script>
 
 <template>
-  <aside
-    class="dark:bg-slate-900 flex justify-center items-center flex-col bg-white"
-  >
-    <nav class="font-poppins">
+  <aside class="flex-center-col bg-white font-poppins dark:bg-slate-900">
+    <nav>
       <ul
-        class="flex flex-col justify-center items-start text-slate-600 dark:text-slate-200 text-2xl gap-4 hover:cursor-pointer"
+        class="flex-start-col gap-4 text-2xl text-slate-600 dark:text-slate-200 hover:cursor-pointer"
       >
-        <li class="text-6xl mb-8 text-emerald-500 font-pacifico mx-auto">
+        <li class="text-6xl mb-4 text-emerald-500 font-pacifico pl-8">
           Howdy<span class="text-orange-400">!</span>
         </li>
-        <li
-          class="bg-orange-400 text-white py-2 px-6 rounded-br-full rounded-tl-full hover:scale-125 duration-150"
-        >
+        <li class="nav-link-orange hover:scale-125 duration-150">
           <button @click="navigateTo('/')">My main projects</button>
         </li>
-        <li
-          class="bg-emerald-500 text-white py-2 px-6 rounded-tr-full rounded-bl-full hover:scale-125 duration-150"
-        >
+        <li class="nav-link-emerald hover:scale-125 duration-150">
           <button @click="navigateTo('/about')">More about me</button>
         </li>
 
-        <li
-          class="bg-orange-400 text-white py-2 px-6 rounded-br-full rounded-tl-full hover:scale-125 duration-150"
-        >
+        <li class="nav-link-orange hover:scale-125 duration-150">
           <button @click="navigateTo('/contact')">Get in touch</button>
         </li>
-        <li
-          class="bg-emerald-500 text-white py-2 px-6 rounded-tr-full rounded-bl-full hover:scale-125 duration-150"
-        >
+        <li class="nav-link-emerald hover:scale-125 duration-150">
           <button @click="toggleDarkMode">Dark Mode</button>
         </li>
-        <li
-          class="bg-orange-400 text-white py-2 px-6 rounded-br-full rounded-tl-full hover:scale-125 duration-150"
-        >
-          Language
-        </li>
+        <li class="nav-link-orange hover:scale-125 duration-150">Language</li>
       </ul>
     </nav>
-    <button class="mt-8 hover:scale-110 duration-300" @click="closeSidebar">
+    <button class="mt-4 hover:scale-110 duration-300" @click="closeSidebar">
       <i class="fa-solid fa-times text-red-400 text-6xl"></i>
     </button>
   </aside>
