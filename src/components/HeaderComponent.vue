5824160f5aae84d7518c7b7374e027575cdd25fe
Juan Lagunas <dev.jlagunas@gmail.com>
2024-06-26 18:23:11 +0900

    refactor(NavbarComponent): simply tailwind classes using @apply directive

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,9 +1,10 @@
 <script setup></script>
 
 <template>
-  <header class="text-center flex justify-center">
-    <p class="font-pacifico text-emerald-500 text-5xl">Hello</p>
-    <p class="font-pacifico text-orange-400 text-5xl animate-bounce">!</p>
+  <header class="text-center">
+    <p class="font-pacifico text-emerald-500 text-5xl">
+      Hello<span class="text-orange-400">!</span>
+    </p>
   </header>
 </template>
 
--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -8,36 +8,21 @@ const toggleDarkMode = () => {
 </script>
 
 <template>
-  <nav class="my-4 w-90vw mx-auto py-2 font-poppins">
-    <ul
-      class="flex justify-around text-slate-600 dark:text-slate-200 lg:justify-center gap-2 text-xs lg:text-base"
-    >
-      <li
-        class="bg-emerald-500 text-white py-1 px-6 rounded-tr-full rounded-bl-full"
-      >
-        <router-link to="/">My Main Projects</router-link>
+  <nav class="my-6 w-90vw mx-auto font-poppins">
+    <ul class="flex justify-center text-white text-xs gap-2 lg:text-lg">
+      <li class="nav-link-emerald">
+        <router-link to="/">Main Projects</router-link>
       </li>
-      <li
-        class="bg-orange-400 text-white py-1 px-6 rounded-tl-full rounded-br-full"
-      >
-        <router-link to="/about">More About Me</router-link>
+      <li class="nav-link-orange">
+        <router-link to="/about">About Me</router-link>
       </li>
-
-      <li
-        class="bg-emerald-500 text-white py-1 px-6 rounded-tr-full rounded-bl-full"
-      >
+      <li class="nav-link-emerald">
         <router-link to="/contact">Get In Touch</router-link>
       </li>
-      <li
-        class="bg-orange-400 text-white py-1 px-6 rounded-tl-full rounded-br-full"
-      >
+      <li class="nav-link-orange">
         <button @click="toggleDarkMode">Dark Mode</button>
       </li>
-      <li
-        class="bg-emerald-500 text-white py-1 px-6 rounded-tr-full rounded-bl-full"
-      >
-        Language
-      </li>
+      <li class="nav-link-emerald"><button>Language</button></li>
     </ul>
   </nav>
 </template>
