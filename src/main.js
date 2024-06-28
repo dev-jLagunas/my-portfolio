bde73980ffd71028ca8c5e271f180f88ba264d7b
Juan Lagunas <dev.jlagunas@gmail.com>
2024-06-28 14:08:46 +0900

    install the vuei18n plugin to prepare for multi lingual functionality

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./assets/main.scss";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
 
 import App from "./App.vue";
 import router from "./router";
