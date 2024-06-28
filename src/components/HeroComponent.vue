f3fc609f73cdba55915e350be5aec94dd430b8ba
Juan Lagunas <dev.jlagunas@gmail.com>
2024-06-28 15:16:29 +0900

    feat(HeroComponent): add translations

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -8,15 +8,15 @@ import LinkButtons from "@/components/LinksBtnsContainer.vue";
     <h3
       class="hidden pacifico-title w-max py-1 ml-auto mt-4 text-3xl lg:block mb-8 xl:text-4xl"
     >
-      Projects<span
-        ><i class="fa-solid fa-arrow-right text-orange-400 pl-4"></i>
+      {{ $t("hero.projects")
+      }}<span>
+        <i class="fa-solid fa-arrow-right text-orange-400 pl-4"></i>
       </span>
     </h3>
     <h1
       class="text-6xl font-bold text-slate-800 dark:text-slate-200 text-center md:absolute md:top-16 md:left-80 lg:static"
     >
-      I<span class="text-orange-400">'</span>m
-      <span class="text-emerald-500">Juan</span>
+      <span v-html="$t('hero.imJuan')"></span>
     </h1>
     <figure
       class="md:flex md:justify-center md:items-center md:gap-4 lg:flex-col"
@@ -29,25 +29,23 @@ import LinkButtons from "@/components/LinksBtnsContainer.vue";
       <figcaption
         class="text-slate-700 text-center px-4 dark:text-slate-200 md:text-xl md:px-0 lg:text-base xl:w-2/3"
       >
-        I'm a
         <span
-          class="text-orange-400 font-semibold text-lg md:text-2xl lg:text-lg"
-          >Frontend Developer</span
-        >
-        and
-        <span
-          class="text-orange-400 font-semibold text-lg md:text-2xl lg:text-lg"
-          >Designer</span
-        >. I enjoy creating simple, user friendly, efficient and vibrant
-        websites.
+          v-html="
+            $t('hero.description', {
+              frontendDeveloper: $t('hero.frontendDeveloper'),
+              designer: $t('hero.designer'),
+            })
+          "
+        ></span>
       </figcaption>
     </figure>
     <LinkButtons class="hidden lg:block" />
     <h3
       class="text-center pacifico-title w-max px-4 py-1 mx-auto my-16 text-4xl lg:hidden"
     >
-      Projects<span
-        ><i class="fa-solid fa-arrow-turn-down text-orange-400 pl-4"></i>
+      {{ $t("hero.projects")
+      }}<span>
+        <i class="fa-solid fa-arrow-turn-down text-orange-400 pl-4"></i>
       </span>
     </h3>
   </section>
