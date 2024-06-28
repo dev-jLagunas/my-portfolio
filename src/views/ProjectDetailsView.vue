4d57ac0164696c2b5f87ec8cee9658a9c510ac37
Juan Lagunas <dev.jlagunas@gmail.com>
2024-06-28 12:19:47 +0900

    refactor(ProjectDetailsView): make layout sizing adjustments to desktop screen sizes

--- src/views/ProjectDetailsView.vue
+++ src/views/ProjectDetailsView.vue
@@ -30,8 +30,8 @@ onMounted(() => {
     v-if="project"
     class="primary-font-colors font-poppins pb-6 mt-4 w-90vw lg:grid lg:grid-cols-8 lg:mx-auto lg:h-screen lg:w-full"
   >
-    <div class="lg:col-span-3 lg:sticky lg:top-0">
-      <h2 class="font-bold text-4xl text-center md:text-6xl lg:text-4xl">
+    <div class="lg:col-span-3 lg:sticky lg:top-0 relative">
+      <h2 class="font-bold text-4xl text-center md:text-6xl lg:text-5xl">
         {{ project.name }}
       </h2>
       <div class="">
@@ -44,24 +44,33 @@ onMounted(() => {
           </button>
         </div>
         <section class="my-4 lg:w-4/5 lg:mx-auto">
-          <p class="project-details-titles md:text-4xl">Description</p>
+          <p class="project-details-titles md:text-3xl">Description</p>
           <p>{{ project.description }}</p>
         </section>
         <section>
-          <p class="project-details-titles md:text-4xl">Made Width</p>
-          <ul class="flex-center-col">
+          <p class="project-details-titles md:text-3xl">Made Width</p>
+          <ul
+            class="flex-center-col md:flex-row md:divide-x-2 md:divide-orange-400 mt-4"
+          >
             <li
               v-for="(technologies, index) in project.technologies"
               :key="index"
+              class="pr-2 pl-2"
             >
               {{ technologies }}
             </li>
           </ul>
         </section>
         <section class="mb-4">
-          <p class="project-details-titles mt-4 md:text-4xl">Features</p>
-          <ul class="flex-center-col">
-            <li v-for="(features, index) in project.features" :key="index">
+          <p class="project-details-titles mt-4 md:text-3xl">Features</p>
+          <ul
+            class="flex-center-col text-center md:flex-row md:divide-x-2 md:divide-orange-400 md:flex-wrap mt-4"
+          >
+            <li
+              v-for="(features, index) in project.features"
+              :key="index"
+              class="px-2"
+            >
               {{ features }}
             </li>
           </ul>
@@ -69,7 +78,7 @@ onMounted(() => {
         <NavButtons class="hidden lg:block" />
       </div>
     </div>
-    <div class="lg:col-span-5 lg:overflow-y-auto">
+    <div class="lg:col-span-5 lg:overflow-y-auto lg:pr-2">
       <figure class="flex-center-col gap-4">
         <figcaption class="project-details-titles md:text-4xl">
           Desktop
@@ -81,18 +90,18 @@ onMounted(() => {
           alt="Desktop screenshot"
         />
       </figure>
-      <figure class="flex-center-col gap-4 mt-4 md:grid md:grid-cols-2">
-        <figcaption class="project-details-titles md:text-4xl">
-          Mobile
-        </figcaption>
-        <img
-          v-for="(image, index) in project.images.mobile"
-          :key="'mobile-' + index"
-          :src="resolvedImages[image]"
-          alt="mobile screenshot"
-          class="w-52 mx-auto custom-shadow"
-        />
-      </figure>
+      <div class="mt-4">
+        <p class="project-details-titles md:text-4xl mb-4">Mobile</p>
+        <figure class="flex-center-col gap-4 md:flex-row lg:flex-wrap">
+          <img
+            v-for="(image, index) in project.images.mobile"
+            :key="'mobile-' + index"
+            :src="resolvedImages[image]"
+            alt="mobile screenshot"
+            class="w-52 mx-auto custom-shadow"
+          />
+        </figure>
+      </div>
       <NavButtons class="mt-8 lg:hidden" />
     </div>
   </section>
