6073b0c55da4381b14979b31ebb2d115793fc74f
Juan Lagunas <dev.jlagunas@gmail.com>
2024-07-03 10:39:14 +0900

    add Formspree for ContactForm.vue component to handle form submission logic

--- src/components/ContactMeForm.vue
+++ src/components/ContactMeForm.vue
@@ -5,11 +5,23 @@
     <h2 class="text-center text-4xl font-bold">
       {{ $t("contactMeForm.title") }}
     </h2>
-    <form class="w-full">
+    <form class="w-full" action="https://formspree.io/f/xzzpbrnb" method="POST">
       <label for="name">{{ $t("contactMeForm.nameLabel") }}</label>
-      <input type="text" id="name" class="w-full border-b" />
+      <input
+        type="text"
+        id="name"
+        name="name"
+        class="w-full border-b"
+        required
+      />
       <label for="email">{{ $t("contactMeForm.emailLabel") }}</label>
-      <input type="email" id="email" class="w-full border-b" />
+      <input
+        type="email"
+        id="email"
+        name="email"
+        class="w-full border-b"
+        required
+      />
       <label for="message">{{ $t("contactMeForm.messageLabel") }}</label>
       <textarea
         name="message"
@@ -17,8 +29,9 @@
         cols="30"
         rows="10"
         class="w-full border-b"
+        required
       ></textarea>
-      <button class="bg-emerald-500 text-white w-full">
+      <button class="bg-emerald-500 text-white w-full" type="submit">
         {{ $t("contactMeForm.submitButton") }}
       </button>
     </form>
